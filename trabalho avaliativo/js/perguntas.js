bdbfc7016d5920654b5c3ebf071ecbbeecaaee2d
euvinicius11 <108193693+euvinicius11@users.noreply.github.com>
2024-08-26 22:05:03 -0300

    Add files via upload

--- trabalho avaliativo/js/perguntas.js
+++ trabalho avaliativo/js/perguntas.js
@@ -5,20 +5,19 @@ export const perguntas = [
             {
                 texto: "Hidrosfera!",
                 afirmacao: [
-                    "Ambas afirmações se completam.",
-                    "Achou assustador pensar como a Hidrosfera completa a outra."
+                    "A atmosfera protege a Terra das radiações solares e mantém o clima adequado para a vida.",
+                    "A atmosfera regula a temperatura global e permite a respiração dos seres vivos, sendo fundamental para a vida na Terra.."
                 ],
                 proxima: 1,
             },
             {
                 texto: "Atmosfera!",
                 afirmacao: [
-                    "Assim ela auxilia a Hidrosfera.",
-                    "Pensou que a Atmosfera interfere muito da sua vida."
+                    "A hidrosfera engloba toda a água do planeta, essencial para a sobrevivência dos seres vivos e dos ecossistemas.",
+                    "A hidrosfera influencia diretamente o clima e o ciclo das chuvas, além de ser a principal fonte de água para todos os organismos.."
                 ],
                 proxima: 2,
             },
         ]
     },
- ]
-        
+]
