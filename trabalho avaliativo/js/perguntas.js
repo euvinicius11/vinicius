cac5d2b1cb5f48c8d0cc54019f8ac1d641ce03b8
euvinicius11 <108193693+euvinicius11@users.noreply.github.com>
2024-08-26 21:56:01 -0300

    Add files via upload

---
+++ trabalho avaliativo/js/perguntas.js
@@ -0,0 +1,24 @@
+export const perguntas = [
+    {
+        enunciado: "Quais são as quatro grandes esferas que compõem o meio ambiente?",
+        alternativas: [
+            {
+                texto: "Hidrosfera!",
+                afirmacao: [
+                    "Ambas afirmações se completam.",
+                    "Achou assustador pensar como a Hidrosfera completa a outra."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Atmosfera!",
+                afirmacao: [
+                    "Assim ela auxilia a Hidrosfera.",
+                    "Pensou que a Atmosfera interfere muito da sua vida."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+ ]
+        
