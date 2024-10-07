182615f0eb7559eedadc9d8dbd3b9459eb8e7732
euvinicius11 <108193693+euvinicius11@users.noreply.github.com>
2024-10-07 09:30:35 -0300

    Add files via upload

---
+++ js/perguntas.js
@@ -0,0 +1,23 @@
+export const perguntas = [
+    {
+        enunciado: "Quais são as quatro grandes esferas que compõem o meio ambiente?",
+        alternativas: [
+            {
+                texto: "Hidrosfera!",
+                afirmacao: [
+                    "A atmosfera protege a Terra das radiações solares e mantém o clima adequado para a vida.",
+                    "A atmosfera regula a temperatura global e permite a respiração dos seres vivos, sendo fundamental para a vida na Terra.."
+                ],
+                proxima: 1,
+            },
+            {
+                texto: "Atmosfera!",
+                afirmacao: [
+                    "A hidrosfera engloba toda a água do planeta, essencial para a sobrevivência dos seres vivos e dos ecossistemas.",
+                    "A hidrosfera influencia diretamente o clima e o ciclo das chuvas, além de ser a principal fonte de água para todos os organismos.."
+                ],
+                proxima: 2,
+            },
+        ]
+    },
+]
